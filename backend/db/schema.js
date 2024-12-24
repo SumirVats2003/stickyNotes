8218718bd912ee407fff2542e049e21c26ee5844
Sumir Vats <sumirvats@gmail.com>
2024-12-24 15:34:44 +0530

    Added DB schemas

---
+++ backend/db/schema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const noteSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+  },
+});
+
+const userDB = mongoose.model("users", userSchema);
+const noteDB = mongoose.model("notes", noteSchema);
+
+export { userDB, noteDB };
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { userDB, noteDB } from "../db/schema.js";
 
 const router = express.Router();
+
+// test route
+router.get("/test", (req, res) => {
+  res.send("This is a test route");
+});
 
 export default router;
